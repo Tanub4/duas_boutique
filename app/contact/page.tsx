0d90d0507d8d91f31f3f1f6c375c2fe5c6c2b807
Tanub4 <tanubks91@gmail.com>
2026-02-12 17:14:14 +0530

    Update map and add public assets

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -80,12 +80,13 @@ export default function ContactPage() {
                         {/* Map */}
                         <div className="bg-white p-2 rounded-xl shadow-md h-[300px] w-full">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3868.5999999999995!2d75.56666666666666!3d13.933333333333334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbbb4e4e4e4e4e5%3A0x4e4e4e4e4e4e4e4e!2sDuas+Boutique!5e0!3m2!1sen!2sin!4v1620000000000!5m2!1sen!2sin"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3872.3899507222895!2d75.5548056!3d13.935371799999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbba9c04e7d25b5%3A0x9e311cb8bc4eedf5!2sDuas%20Boutique!5e0!3m2!1sen!2sin!4v1770896561871!5m2!1sen!2sin"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0, borderRadius: '0.75rem' }}
                                 allowFullScreen
                                 loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade"
                             ></iframe>
                         </div>
 
