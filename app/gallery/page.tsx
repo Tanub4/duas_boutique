8e5a587ddd03b2ef4142b311fe0205ed39d88d73
Tanub4 <tanubks91@gmail.com>
2026-02-12 16:41:48 +0530

    Update gallery image

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -4,7 +4,7 @@ const images = [
     { src: "https://images.unsplash.com/photo-1595777457583-95e059d581b8?q=80&w=2583&auto=format&fit=crop", category: "Bridal" },
     { src: "https://images.unsplash.com/photo-1566174053879-31528523f8ae?q=80&w=2548&auto=format&fit=crop", category: "Gowns" },
     { src: "https://images.unsplash.com/photo-1585487000160-6ebcfceb0d03?q=80&w=2534&auto=format&fit=crop", category: "Fabric" },
-    { src: "https://images.unsplash.com/photo-1551488852-d775567637e9?q=80&w=2670&auto=format&fit=crop", category: "Custom" },
+    { src: "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=2670&auto=format&fit=crop", category: "Custom" },
     { src: "https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?q=80&w=2670&auto=format&fit=crop", category: "Designer" },
     { src: "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=2670&auto=format&fit=crop", category: "Workshop" },
 ];
